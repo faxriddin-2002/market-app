16c845b4cde6d4ae140866712913cb68e0bf4943
unknown <sodiqofaxriddin6@gmail.com>
2021-07-05 08:38:57 +0500

    app-interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { HeaderComponent } from './header/header.component';
+import { RenderComponent } from './render/render.component';
+import { RightComponent } from './right/right.component';
+import { OrderStatusComponent } from './order-status/order-status.component';
+import { AddOrderComponent } from './add-order/add-order.component';
+
+
+const firs =[
+  {path:"render", component: RenderComponent},
+  {path:"right", component: RightComponent},
+  {path: "order-status", component: OrderStatusComponent},
+  {path: "add-order", component: AddOrderComponent},
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    HeaderComponent,
+    RenderComponent,
+    RightComponent,
+    OrderStatusComponent,
+    AddOrderComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    RouterModule.forRoot(firs)
   ],
   providers: [],
   bootstrap: [AppComponent]
